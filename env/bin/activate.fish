51d68688d463e96d770fa5598185390922c6a9e9
doublerch <doublerch@gmail.com>
2022-04-28 21:12:05 -0500

    lab05/hw03

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ryanchen/Desktop/cs-webdev/webdev-assignments/photo-app/env"
+set -gx VIRTUAL_ENV "/Users/ryanchen/Desktop/cs-webdev/webdev-assignments/doublerch-photo-app/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
